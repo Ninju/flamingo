ee7680fe84a5e2a4aefa83e7e5d6757a997767f9
Nicholas Wright <nick.loves.rails@gmail.com>
2010-03-02 01:23:52 -0700

    add typings to server functions

--- Flamingo/Server.hs
+++ Flamingo/Server.hs
@@ -15,12 +15,15 @@ portNumber = 3333
 handle :: Connection -> Handle
 handle (h, _, _) = h
 
+setupAndAcceptConnections :: Socket -> IO b
 setupAndAcceptConnections = acceptConnections
 
+acceptConnections :: Socket -> IO b
 acceptConnections socket = do connection <- accept socket
                               forkIO $ (handleClient connection `finally` hClose (handle connection))
                               acceptConnections socket
 
+handleClient :: (Handle, t, t1) -> IO b
 handleClient connection@(handle,_,_) = do hPutStr handle prompt
                                           hFlush handle
                                           input <- hGetLine handle
