c65d3e1980611381003d545fc6cbd744f91e0e75
Alex Watt <alex.watt@me.com>
2010-03-02 19:19:43 +0000

    Added move command

--- Flamingo/Commands.hs
+++ Flamingo/Commands.hs
@@ -2,15 +2,20 @@ module Flamingo.Commands where
 import System.IO
 import Control.Concurrent.STM
 import Time (getClockTime, toUTCTime, calendarTimeToString)
+import Flamingo.Rooms (description, move, isExit)
 
 currentTime :: IO String
 currentTime = do t <- getClockTime
                  return $ calendarTimeToString (toUTCTime t)
 
 command :: [String] -> IO String
-command ("time":_) = currentTime
-command ("look":_) = return "You see an empty room, waiting to be filled."
-command _          = return "Invalid command"
+command _ _ ("time":_) = currentTime
+command _ _ ("look":_) = return "You see an empty room, waiting to be filled."
+command _ r ("move":d) = if isExit d r
+                           then do move d r
+                                   return (description r)
+                           else return "You can't go that way."
+command _ _ _          = return "Invalid command"
 
 execute :: (Handle, a, b) -> TVar c -> String -> IO ()
-execute (handle,_,_) _ input = (command . words) input >>= hPutStrLn handle
+execute connection@(handle,_,_) currentRoom input = command connection currentRoom input >>= hPutStrLn handle
