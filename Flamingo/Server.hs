97aa5aeb8b582a3ceb99902d829116524af0220c
Alex Watt <alex.watt@me.com>
2010-03-02 13:53:52 +0000

    Added currentRoom TVar

--- Flamingo/Server.hs
+++ Flamingo/Server.hs
@@ -2,6 +2,7 @@ module Flamingo.Server where
 import Network
 import System.IO
 import Control.Concurrent
+import Control.Concurrent.STM
 import Control.Exception hiding (handle)
 import Flamingo.Commands (execute)
 
@@ -17,20 +18,20 @@ handle :: Connection -> Handle
 handle (h, _, _) = h
 
 setupAndAcceptConnections :: Socket -> IO b
-setupAndAcceptConnections socket = do currentRoom <- newTVar "start"
+setupAndAcceptConnections socket = do currentRoom <- newTVarIO "start"
                                       acceptConnections socket currentRoom
 
-acceptConnections :: Socket -> IO b
+acceptConnections :: Socket -> TVar a -> IO b
 acceptConnections socket currentRoom = do connection <- accept socket
                                           forkIO $ (handleClient connection currentRoom `finally` hClose (handle connection))
-                                          acceptConnections socket
+                                          acceptConnections socket currentRoom
 
-handleClient :: Connection -> IO a
+handleClient :: Connection -> TVar a -> IO b
 handleClient connection@(handle,_,_) currentRoom = do hPutStr handle prompt
                                                       hFlush handle
                                                       input <- hGetLine handle
                                                       response <- (execute input)
                                                       hPutStrLn handle response
-                                                      handleClient connection
+                                                      handleClient connection currentRoom
 
 run = bracket (listenOn $ PortNumber portNumber) sClose setupAndAcceptConnections
