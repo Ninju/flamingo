9b796e95a5e513a32fb9b1e4e84509135dc58ee2
Alex Watt <alex.watt@me.com>
2010-03-03 17:54:00 +0000

    Branch 4 done I think

--- Flamingo/Commands.hs
+++ Flamingo/Commands.hs
@@ -2,18 +2,13 @@ module Flamingo.Commands where
 import Control.Concurrent.STM
 import Control.Monad.Reader
 import System.IO
-import Time (getClockTime, toUTCTime, calendarTimeToString)
 import Flamingo.Rooms
 import Flamingo.Utils
-
-currentTime :: IO String
-currentTime = getClockTime >>= return . calendarTimeToString . toUTCTime
 
 move :: Direction -> Room -> ReaderT Environment IO Room
 move direction room = ask >>= \env -> lift $ maybe (return room) (flip runReaderT env . setCurrentRoom) $ lookup direction (exits room)
 
 command :: [String] -> ReaderT Environment IO String
-command ("time":_)   = liftIO $ currentTime
 command ("look":_)   = asks currentRoom >>= lift . atomically . readTVar >>= return . (++ "\n") . show
 command ("move":[])  = return "Enter a direction in which to move."
 command ("move":d:_) = do tvR <- asks currentRoom
