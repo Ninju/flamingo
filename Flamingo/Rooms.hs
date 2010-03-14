1213a9ec8d926d455a269339cb23ee321426bb2d
Alex Watt <alex.watt@me.com>
2010-03-14 13:40:29 +0000

    Added inhabitants member onto Room data type

--- Flamingo/Rooms.hs
+++ Flamingo/Rooms.hs
@@ -5,13 +5,18 @@ import Data.Char (toLower)
 import Data.List (intercalate)
 
 data Direction = North | East | South | West deriving (Eq, Show, Enum)
-data Room = Room { exits :: [(Direction, Room)], description :: String }
+newtype Inhabitant = Name String
+data Room = Room { exits :: [(Direction, Room)], description :: String, inhabitants :: [Inhabitant] }
 
 startingRoom :: Room
-startingRoom = Room { exits = [(North, crampedCloset)], description = "You find yourself in a round room with a pillar in the middle." }
+startingRoom = Room { exits = [(North, crampedCloset)],
+                      description = "You find yourself in a round room with a pillar in the middle.",
+                      inhabitants = [] }
 
 crampedCloset :: Room
-crampedCloset = Room { exits = [(South, startingRoom)], description = "You are in a cramped closet." }
+crampedCloset = Room { exits = [(South, startingRoom)],
+                       description = "You are in a cramped closet.",
+                       inhabitants = [] }
 
 instance Show Room where
   show r = description r ++ "\nExits: (" ++ intercalate ", " (map (map toLower . show . fst) $ exits r) ++ ")"
