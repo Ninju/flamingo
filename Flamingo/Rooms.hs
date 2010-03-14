fee1b1e20fd108ef73efff13408d1689434de778
Alex Watt <alex.watt@me.com>
2010-03-14 12:28:29 +0000

    fixed instance dec for Flamingo.Rooms to use lowercase exit names (i.e. 'north', 'south', not 'North', 'South')

--- Flamingo/Rooms.hs
+++ Flamingo/Rooms.hs
@@ -3,7 +3,8 @@ module Flamingo.Rooms (Room(Room), exits, description,
                        startingRoom, crampedCloset) where
 import Control.Concurrent.STM
 import Control.Monad.Reader
-import Data.List
+import Data.Char (toLower)
+import Data.List (intercalate)
 import System.IO
 import Flamingo.Utils
 
@@ -17,4 +18,4 @@ crampedCloset :: Room
 crampedCloset = Room { exits = [(South, startingRoom)], description = "You are in a cramped closet." }
 
 instance Show Room where
-  show r = description r ++ "\nExits: (" ++ intercalate ", " (map (show . fst) $ exits r) ++ ")"
+  show r = description r ++ "\nExits: (" ++ intercalate ", " (map (map toLower . show . fst) $ exits r) ++ ")"
